44ea4d67409c6a36130c3f758a1ab76bd45c2b41
thruttle <thruttle@gmail.com>
2014-03-11 09:21:09 +0000

    rename the things

--- learnyounode/3firstio.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs');
-var filepath = process.argv[2];
-
-if (!filepath) return;
-
-var readfile = fs.readFileSync(filepath).toString();
-var readfilearray = readfile.split('\n');
-
-console.log(readfilearray.length - 1);
--- learnyounode/4firstasync.js
+++
@@ -1,8 +0,0 @@
-var fs = require('fs');
-var filepath = process.argv[2];
-
-if (!filepath) return;
-
-var readfile = fs.readFile(filepath, 'utf8', function(err, data) {
-	console.log(data.split('\n').length -1);n
-});
